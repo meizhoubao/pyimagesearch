26969a69715c711b6741f2198f40b28cb3aae98d
meizhoubao <18032773381@163.com>
2017-03-06 21:21:09 +0800

    Add detect circles

---
+++ detect-circles/detect_circles.py
@@ -0,0 +1,20 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+output = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 75)
+if circles is not None:
+    circles = np.round(circles[0, :]).astype("int")
+    for (x, y, r) in circles:
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    cv2.imshow("output", np.hstack([image, output]))
+    cv2.waitKey(0)
