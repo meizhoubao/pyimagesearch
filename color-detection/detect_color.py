e3d24da8e3ea8a73074cc68e7d800ca00413f89c
meizhoubao <18032773381@163.com>
2017-03-17 21:31:08 +0800

    Add color detection

---
+++ color-detection/detect_color.py
@@ -0,0 +1,26 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+boundaries = [
+    ([17, 15, 100], [50, 56, 200]),
+    ([86, 31, 4], [220, 88, 50]),
+    ([25, 146, 190], [62, 174, 250]),
+    ([103, 86, 65], [145, 133, 128])
+    ]
+
+for (lower, upper) in boundaries:
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
