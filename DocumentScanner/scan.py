1bdb2799d3393133069d5f97bcb288e4b426a059
meizhoubao <18032773381@163.com>
2017-05-18 17:38:47 +0800

    Add document scanner

---
+++ DocumentScanner/scan.py
@@ -0,0 +1,54 @@
+from pyimagesearch.transform import four_point_transform
+from pyimagesearch import imutils
+from skimage.filters import threshold_adaptive
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height=500)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edge", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    if (len(approx) == 4):
+        screenCnt = approx
+        break
+
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+warped = threshold_adaptive(warped, 251, offset=10)
+warped = warped.astype("uint8") * 255
+
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height=650))
+cv2.imshow("Scanned", imutils.resize(warped, height=650))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
